3309492810ad4a68bf735bd4d2118367b2e7984e
shi0n0 <shiono.yui1080@gmail.com>
2023-03-21 21:47:51 +0900

    Add Task

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -20,16 +20,35 @@ const TodoList = () => {
     ]
 
     const [todos,setTodos] = useState(initialState);
-    
+    const [task,setTask] = useState('')
+
+    const handleNewTask = (event) => {
+        setTask(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        if (task === '')
+        return
+        setTodos(todos => [...todos,{ task, isCompleated: false}])
+        setTask('')
+
+    }
     return(
-        <div>
+        <form onSubmit={handleSubmit}>
             <h1>Todo List</h1>
+            タスクの追加 : <input
+                            value={task}
+                            placeholder="新しいタスク"
+                            onChange={handleNewTask}
+                            />
+            <button type="submit">追加</button>
             <ul>
                 { todos.map((todo,index) => (
                     <li key={ index }>{ todo.task }</li>
                 ))}
             </ul>
-        </div>
+        </form>
     );
 }
 
