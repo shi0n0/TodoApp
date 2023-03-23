40d7787a95f34bba2043d5a7be835e579f257343
shi0n0 <shiono.yui1080@gmail.com>
2023-03-24 00:14:50 +0900

    componentization

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,84 +1,37 @@
-import { useState } from "react";
-const TodoList = () => {
-
-    const initialState = [
-        {
-            task: "Reactを学ぶ",
-            isCompleted: false
-        },
-
-        {
-            task: "Golangを学ぶ",
-            isCompleted: false
-        },
-
-        {
-            task: "GraphQLを学ぶ",
-            isCompleted: false
-        },
-    ]
-
-    const [todos,setTodos] = useState(initialState);
-    const [task,setTask] = useState('')
-
-    const handleNewTask = (event) => {
-        setTask(event.target.value)
-    }
-
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        if (task === '')
-        return
-        setTodos(todos => [...todos,{ task, isCompleted: false}])
-        setTask('')
-    }
-
+import React from "react";
+const TodoList = ({todos,setTodos}) => {
     const handleTaskCompleated = (index) => {
         const newTodos = [...todos]
         newTodos[index].isCompleted = !newTodos[index].isCompleated
         setTodos(newTodos)
-    }
+    };
 
     const handleRemoveTask = (index) => {
         const NewTodos = [...todos]
         NewTodos.splice(index,1)
         setTodos(NewTodos)
-    }
-
-
-
-
+    };
 
     return(
-        <form onSubmit={handleSubmit}>
-            <h1>Todo List</h1>
-            タスクの追加 : <input
-                            value={task}
-                            placeholder="新しいタスク"
-                            onChange={handleNewTask}
-                            />
-            <button type="submit" hidden>追加</button>
-            <ul>
-                { todos.map((todo,index) => (
-                    <li key={ index }      
-                    style={{
-                        textDecoration: todo.isCompleted ? 'line-through' : 'none',
-                    }}>
-                        <label>
-                            <input
-                                type={"checkbox"}
-                                checked={todo.isCompleted}
-                                onChange={ () => handleTaskCompleated(index) }
-                            />
-                        </label>
-                        { todo.task } 
-                        <button onClick={ () => handleRemoveTask(index) }>削除</button>
-                    <button></button>
-                    </li>
-                ))}
-            </ul>
-        </form>
+        <ul>
+            { todos.map((todo,index) => (
+                <li key={ index }      
+                style={{
+                    color: todo.isCompleted ? 'green' : 'initial',
+                }}>
+                    <label>
+                        <input
+                            type={"checkbox"}
+                            checked={todo.isCompleted}
+                            onChange={ () => handleTaskCompleated(index) }
+                        />
+                    </label>
+                    { todo.task } 
+                    <button onClick={ () => handleRemoveTask(index) }>削除</button>
+                </li>
+            ))}
+        </ul>
     );
-}
+};
 
 export default TodoList;
