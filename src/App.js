55453296d4f64ae18dce510663303286a30bec34
shi0n0 <shiono.yui1080@gmail.com>
2023-03-25 23:34:47 +0900

    Easy design changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import Todo from './components/Todo';
-
+import './App.css'
 function App() {
   return (
-    <div style={{ margin: "2em"}}>
+    <>
+    <header>
+      <a href='/'>
+        <h1 className='Title'>Todo List</h1>
+      </a>
+    </header>
+    <div className='container'>
       <Todo />
     </div>
+    </>
   );
 }
 
